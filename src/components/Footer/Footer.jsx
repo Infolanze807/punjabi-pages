19cf2b7540a834bbd290bdced2d85ca25905c2af
nishant-infolanze <nishant.infolanze@gmail.com>
2025-06-11 18:28:01 +0530

    Live

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -207,7 +207,7 @@ export function Footer() {
             </div>
 
             {/* Social & Contact Column */}
-            <div>
+            <div> 
               <h3 className="text-xl font-bold text-white mb-6">Connect</h3>
               <ul className="space-y-3 mb-6">
                 <li>
